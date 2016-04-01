7656ded501b1997065d0ee3f29ace11ef148b351
Eric Rochester <erochest@virginia.edu>
2016-04-01 11:23:18 -0400

    No longer failing spectacularly.

--- testscript.py
+++ testscript.py
@@ -2,7 +2,8 @@
 
 
 import csv
-import re
+import os
+import sys
 
 import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey, Table
@@ -15,6 +16,38 @@ Base = declarative_base()
 
 CSV_FILE = 'Apulian_Database_dates.csv'
 DB_NAME = 'apulian.sqlite'
+
+
+def take_digits(input):
+    """\
+    This strips of the digits from the beginning of the string and returns as
+    an int. The also returns the rest of the input.
+
+    >>> take_digits('abc')
+    (None, 'abc')
+    >>> take_digits('1abc')
+    (1, 'abc')
+    >>> take_digits('42xyz')
+    (42, 'xyz')
+
+    """
+
+    accum = []
+    rest = input
+
+    for i in range(len(input)):
+        if input[i].isdigit():
+            accum.append(input[i])
+        else:
+            rest = input[i:]
+            break
+
+    if not accum:
+        n = None
+    else:
+        n = int(''.join(accum))
+
+    return (n, rest)
 
 
 class Vase(Base):
@@ -93,16 +126,11 @@ class Image(Base):
 # apply to multiple sides, and each side can have multiple themes represented.
 # In the database, the relationship only exists in this table, not in the
 # tables it references.
-side_theme = Table('side_theme', Base.metadata,
-                   Column('side_id', ForeignKey('sides.id'), primary_key=True),
-                   Column('theme_id', ForeignKey('themes.id'),
-                          primary_key=True))
-
-side_musical_theme = Table('side_musical_theme', Base.metadata,
-                           Column('side_id', ForeignKey('sides.id'),
-                                  primary_key=True),
-                           Column('theme_id', ForeignKey('musical_themes.id'),
-                                  primary_key=True))
+side_theme = Table(
+    'side_theme', Base.metadata,
+    Column('side_id', ForeignKey('sides.id'), primary_key=True),
+    Column('theme_id', ForeignKey('themes.id'), primary_key=True),
+    )
 
 
 class Side(Base):
@@ -118,19 +146,13 @@ class Side(Base):
     vase_id = Column(Integer, ForeignKey('vases.id'))
     vase = relationship('Vase', back_populates='sides')
 
-    # C.2. This is one end of the many-to-many relationship.
+    # C.2. Each side links to an unspecified number of themes.
     themes = relationship(
         'Theme',
         secondary=side_theme,
         back_populates='sides',
     )
     figures = relationship('Figure', back_populates='side')
-
-    musical_themes = relationship(
-        'MusicalTheme',
-        secondary=side_musical_theme,
-        back_populates='sides',
-    )
     instruments = relationship(
         'InstrumentInstance',
         back_populates='side',
@@ -150,40 +172,15 @@ class Theme(Base):
         back_populates='themes',
     )
 
+    instruments = relationship(
+        'InstrumentInstance',
+        back_populates='theme'
+    )
+
     def __repr__(self):
         return '<Theme id={} name={}>'.format(
             self.id, self.name,
         )
-
-
-musical_theme_instrument = Table(
-    'musical_theme_instrument', Base.metadata,
-    Column('musical_theme_id', ForeignKey('musical_themes.id'),
-           primary_key=True),
-    Column('instrument_id', ForeignKey('instruments.id'), primary_key=True),
-)
-
-
-class MusicalTheme(Base):
-    __tablename__ = 'musical_themes'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(20))
-
-    sides = relationship(
-        'Side',
-        secondary=side_musical_theme,
-        back_populates='musical_themes',
-    )
-    instrument_types = relationship(
-        'Instrument',
-        secondary=musical_theme_instrument,
-        back_populates='themes',
-    )
-    instrument_instances = relationship(
-        'InstrumentInstance',
-        back_populates='theme'
-    )
 
 
 class Instrument(Base):
@@ -192,12 +189,10 @@ class Instrument(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(20))
 
-    themes = relationship(
-        'MusicalTheme',
-        secondary=musical_theme_instrument,
-        back_populates='instrument_types',
-    )
-    instances = relationship('InstrumentInstance', back_populates='instrument')
+    instances = relationship(
+        'InstrumentInstance',
+        back_populates='instrument',
+        )
 
 
 class InstrumentInstance(Base):
@@ -208,16 +203,15 @@ class InstrumentInstance(Base):
     performer = Column(String(15))
     location = Column(String(10))
     action = Column(String(20))
-    instr_count = Column(Integer)
 
     side_id = Column(Integer, ForeignKey('sides.id'))
     side = relationship('Side', back_populates='instruments')
 
+    theme_id = Column(Integer, ForeignKey('themes.id'))
+    theme = relationship('Theme', back_populates='instruments')
+
     instrument_id = Column(Integer, ForeignKey('instruments.id'))
     instrument = relationship('Instrument', back_populates='instances')
-
-    theme_id = Column(Integer, ForeignKey('musical_themes.id'))
-    theme = relationship('MusicalTheme', back_populates='instrument_instances')
 
 
 class Figure(Base):
@@ -233,18 +227,13 @@ class Figure(Base):
 
 def read_csv(filename):
     """This reads the CSV files. """
-    with open(filename) as f:
+    with open(filename, encoding='latin1') as f:
         reader = csv.DictReader(f)
         for row in reader:
             yield row
 
 
-# SCENE_TYPE_B,
-# MUSICAL_SCENE_TYPE_B,TRENDALL_DESCRIPTION_B,SIDE_B_DETAILS,
-# SIDE_B_INSTRUMENTS_AND_NUMBERS,SIDE_B_PERFORMERS,SIDE_B_PERFORMER_LOCATION,
-# SIDE_B_PERFORMER_ACTION,SIDE_B_NUMBER_OF_FIGURES,SIDE_B_COMPOSITION,
-
-# IMAGE_IDS,NOTES,PUBLICATION,CATEGORY_1,CATEGORY_2
+# TODO: IMAGE_IDS,NOTES,PUBLICATION,CATEGORY_1,CATEGORY_2
 class RowAdapter:
 
     def __init__(self):
@@ -252,46 +241,55 @@ class RowAdapter:
         self.painters = {}
         self.locations = {}
         self.themes = {}
-        self.musical_themes = {}
+        self.instruments = {}
 
     def adapt_vase(self, row):
         """Adapts a CSV row into a sequence of database objects."""
         objects = []
 
-        trendall_ch, trendall_no = self._parse_trendall(row['TRENDALL_ID'])
+        try:
+            trendall_ch, trendall_no = self._parse_trendall(row['TRENDALL_ID'])
 
-        vase = Vase(
-            fabric=row['FABRIC'],
-            form=row['FORM'],
-            subform=row['SUB-FORM'],
-            produced_start=row['START_DATE'],
-            produced_end=row['END_DATE'],
-            provenience=row['PROVENIENCE'],
-            trendall_ch=trendall_ch,
-            trendall_no=trendall_no,
-        )
-        objects.append(vase)
-        self.vases.append(vase)
+        except:
+            print('INVALID TRENDALL ID: "{}"'.format(row['TRENDALL_ID']))
 
-        vase.painter = self._get_cached(
-            self.painters, objects, row['PAINTER'],
-            lambda: Painter(name=row['PAINTER'])
-        )
-        vase.location = self._get_cached(
-            self.locations, objects, row['LOCATION_CITY'],
-            lambda: Location(
-                city_name=row['LOCATION_CITY'],
-                collection_name=row['COLLECTION_NAME'],
-                collection_id=row['COLLECTION_ID'],
-            ),
-        )
+        else:
+            vase = Vase(
+                fabric=row['FABRIC'],
+                form=row['FORM'],
+                subform=row['SUB-FORM'],
+                produced_start=row['START_DATE'],
+                produced_end=row['END_DATE'],
+                provenience=row['PROVENIENCE'],
+                trendall_ch=trendall_ch,
+                trendall_no=trendall_no,
+            )
+            objects.append(vase)
+            self.vases.append(vase)
 
-        for image_id in self._parse_images(row['IMAGE_SERIES']):
-            image = Image(name=str(image_id), vase=vase)
-            objects.append(image)
+            vase.painter = self._get_cached(
+                self.painters, objects, row['PAINTER'],
+                lambda: Painter(name=row['PAINTER'])
+            )
+            vase.location = self._get_cached(
+                self.locations, objects, row['LOCATION_CITY'],
+                lambda: Location(
+                    city_name=row['LOCATION_CITY'],
+                    collection_name=row['COLLECTION_NAME'],
+                    collection_id=row['COLLECTION_ID'],
+                ),
+            )
 
-        vase.sides.append(self._adapt_side('A', row, objects))
-        vase.sides.append(self._adapt_side('B', row, objects))
+            for image_id in self._parse_images(row['IMAGE_SERIES']):
+                image = Image(name=str(image_id), vase=vase)
+                objects.append(image)
+
+            vase.sides.append(
+                self._adapt_side('A', row, objects, row['TRENDALL_ID']),
+                )
+            vase.sides.append(
+                self._adapt_side('B', row, objects, row['TRENDALL_ID']),
+                )
 
         return objects
 
@@ -306,22 +304,30 @@ class RowAdapter:
 
         for number_range_str in image_str.split(','):
             number_range = number_range_str.split('-')
-            if len(number_range) == 1:
-                numbers.add(int(number_range))
+            # This is because there is at least one like "123-"
+            if len(number_range) == 1 or not number_range[1]:
+                if number_range[0].isdigit():
+                    numbers.add(int(number_range[0]))
             else:
-                start_str, end_str = number_range
-                start = int(start_str)
-                end = int(end_str)
-                if end < start:
-                    end = int(start_str[:-len(end_str)] + end_str)
-                numbers.add(start)
-                numbers.add(end)
+                try:
+                    start_str, end_str = number_range
+                    start = int(start_str)
+                    # This split and index protects from things like
+                    # "1 (actual)"
+                    end = int(end_str.split()[0])
+
+                    if end < start:
+                        end_str = start_str[:-len(end_str)] + end_str
+                        end = int(end_str.split()[0])
+                    numbers.add(start)
+                    numbers.add(end)
+                except:
+                    print('ERROR on "{}"'.format(image_str))
+                    raise
 
         return sorted(numbers)
 
-# SIDE_A_INSTRUMENTS_AND_NUMBERS,SIDE_A_PERFORMERS,SIDE_A_PERFORMER_LOCATION,
-# SIDE_A_PERFORMER_ACTION,SIDE_A_NUMBER_OF_FIGURES,SIDE_A_COMPOSITION,
-    def _adapt_side(self, side_id, row, objects):
+    def _adapt_side(self, side_id, row, objects, trendall_id):
         side_data = self._get_side_data(row, side_id)
 
         side = Side(
@@ -330,51 +336,95 @@ class RowAdapter:
             details=side_data['details'],
             catalogue=side_data['catalogue_description'],
         )
+        objects.append(side)
 
-        for theme_name in side_data['scene_type']:
+        # First, let's make sure the themes and instruments we want to connect
+        # already exist.
+        side_themes = {}
+        for (theme_name, _) in side_data['scene_type']:
             theme = self._get_cached(
                 self.themes, objects, theme_name,
                 lambda: Theme(name=theme_name),
             )
-            side.themes.append(theme)
-        for theme_name in side_data['musical_scene_type']:
-            theme = self._get_cached(
-                self.musical_themes, objects, theme_name,
-                lambda: MusicalTheme(name=theme_name),
+            side_themes[theme_name] = theme
+        side.themes.extend(side_themes.values())
+        for (_, inst_name) in side_data['musical_scene_type']:
+            self._get_cached(
+                self.instruments, objects, inst_name,
+                lambda: Instrument(name=inst_name),
             )
-            side.musical_themes.append(theme)
+
+        inst_info = list(zip(
+            side_data['musical_scene_type'],
+            side_data['performers'],
+            side_data['performer_location'],
+            side_data['performer_action'],
+            ))
+        for (ms, p, pl, pa) in inst_info:
+            if ms[0] and ms[1] and ms[1] == p[1] == pl[1] == pa[1]:
+                inst_inst = InstrumentInstance(
+                    performer=p[0],
+                    location=pl[0],
+                    action=pa[0],
+                    side=side,
+                    theme=self.themes[ms[0]],
+                    instrument=self.instruments[ms[1]],
+                    )
+                objects.append(inst_inst)
+            elif not (ms[1] == p[1] == pl[1] == pa[1]):
+                print("WARNING [{}, {}]: different instruments: "
+                      "{} != {} != {} != {}\n\t{}".format(
+                          trendall_id, side_id,
+                          ms, p, pl, pa, inst_info,
+                          ))
+            elif p[0]:
+                print("WARNING [{}, {}]: missing musical scene type."
+                      .format(trendall_id, side_id))
+            elif not (ms[0] or ms[1]):
+                continue
+            else:
+                print("WARNING [{}, {}]: indeterminate error: "
+                      "{} != {} != {} != {}\n\t{}".format(
+                          trendall_id, side_id,
+                          ms, p, pl, pa, inst_info,
+                          ))
 
         # figures
-        # instrument instance
+        for (fig_type, fig_count) in side_data['figure_count']:
+            figure = Figure(
+                figure_type=fig_type,
+                figure_count=fig_count,
+                side=side,
+                )
+            objects.append(figure)
 
         return side
 
     def _get_side_data(self, row, side_id):
         return {
             'scene_type': self._parse_scene_type(
-                row['SCENE_TYPE_' + side_id]),
-
-            # parse like scene_type, pull off last component and make an
-            # Instrument, associated with both MusicalThemes that precede it.
-
+                row['SCENE_TYPE_' + side_id], False),
             'musical_scene_type': self._parse_scene_type(
-                row['MUSICAL_SCENE_TYPE_' + side_id]),
-            'catalogue_description': row['CATALOGUE_DESCRIPTION_' + side_id],
+                row['MUSICAL_SCENE_TYPE_' + side_id], True),
+            'catalogue_description': row[
+                'SIDE_{}_DETAILS'.format(side_id)],
+            # 'CATALOGUE_DESCRIPTION_' + side_id],
             'details': row['SIDE_%s_DETAILS' % side_id],
-
-            # throw away these numbers, get them by counting occurrences in
-            # performers column
-
             'instruments_and_numbers': self._parse_instr_nos(
                 row['SIDE_%s_INSTRUMENTS_AND_NUMBERS' % side_id]),
-            # PERFORMER(INST)
-            'performers': row['SIDE_%s_PERFORMERS' % side_id],
-            # LOCATION(INST)
-            'performer_location': row['SIDE_%s_PERFORMER_LOCATION' % side_id],
-            # ACTION(INST)
-            'performer_action': row['SIDE_%s_PERFORMER_ACTION' % side_id],
+            'performers': self._parse_scene_type(
+                row['SIDE_%s_PERFORMERS' % side_id], True,
+                ),
+            'performer_location': self._parse_scene_type(
+                row['SIDE_%s_PERFORMER_LOCATION' % side_id], True,
+                ),
+            'performer_action': self._parse_scene_type(
+                row['SIDE_%s_PERFORMER_ACTION' % side_id], True,
+                ),
             # COUNT_FIGURE
-            'figure_count': row['SIDE_%s_NUMBER_OF_FIGURES' % side_id],
+            'figure_count': self._parse_instr_nos(
+                row['SIDE_%s_NUMBER_OF_FIGURES' % side_id],
+                ),
             'composition': row['SIDE_%s_COMPOSITION' % side_id],
         }
 
@@ -385,20 +435,21 @@ class RowAdapter:
 
         >>> adapter = RowAdapter()
         >>> adapter._parse_scene_type('PROCESSION')
-        ['PROCESSION']
+        [('PROCESSION', None)]
         >>> adapter._parse_scene_type('PROCESSION (DIONYSIAC), MYTHOLOGICAL')
-        ['PROCESSION', 'DIONYSIAC', 'MYTHOLOGICAL']
+        [('PROCESSION', None), ('DIONYSIAC', None), ('MYTHOLOGICAL', None)]
 
         If with_trailing is True, then each item in the list is a tuple
         pair of the item and the trailing parenthesis for that comma-
         separated segment.
 
-        >>> adapter._parse_scene_type('PROCESSION')
+        >>> adapter._parse_scene_type('PROCESSION', True)
         [('PROCESSION', None)]
-        >>> adapter._parse_scene_type('PROCESSION(AU)')
+        >>> adapter._parse_scene_type('PROCESSION(AU)', True)
         [('PROCESSION', 'AU')]
-        >>> adapter._parse_scene_type(
-                'ATTENDANT (DIONYSIAC)(TYM), ATTENDANT (DIONYSIAC)(CYM)'
+        >>> adapter._parse_scene_type(\
+                'ATTENDANT (DIONYSIAC)(TYM), ATTENDANT (DIONYSIAC)(CYM)',\
+                True,\
                 )
         [('ATTENDANT', 'TYM'), ('DIONYSIAC', 'TYM'), ('ATTENDANT', 'CYM'), ('DIONYSIAC', 'CYM')]
 
@@ -410,23 +461,29 @@ class RowAdapter:
                     for tname in s_type.split('(')
                     ]
             if with_trailing:
-                if len(theme_names) > 1:
-                    trailing = theme_names.pop()
-                else:
-                    trailing = None
-                theme_names = [(tname, trailing) for tname in theme_names]
+                trailing = theme_names.pop() if len(theme_names) > 1 else None
+            else:
+                trailing = None
+            names += [(tname, trailing) for tname in theme_names]
         return names
-
-    def _parser_perf_info(self, performer_info):
-        pass
 
     def _parse_instr_nos(self, value):
         inos = []
         for ino in value.split(','):
+            if not ino or '_' not in ino:
+                continue
             count, instr = ino.split('_')
-            count = int(count)
-            instr = instr.strip('()')
-            inos.append((instr, count))
+            if count:
+                if count[0].isdigit() and not count.isdigit():
+                    count, instr = take_digits(count)
+                else:
+                    try:
+                        count = int(count)
+                    except:
+                        print("int ERROR on '{}'".format(ino))
+                        raise
+                instr = instr.strip('()')
+                inos.append((instr, count))
         return inos
 
     def _get_cached(self, cache, objects, key, ctor):
@@ -442,6 +499,10 @@ class RowAdapter:
 
 
 def main():
+    if '-X' in sys.argv or '--clear' in sys.argv:
+        print('Removing {}'.format(DB_NAME))
+        os.remove(DB_NAME)
+
     engine = sqlalchemy.create_engine(
         'sqlite:///{}'.format(DB_NAME), echo=True
     )
